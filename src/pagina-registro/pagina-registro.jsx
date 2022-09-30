7df922ae706b3022f36f160211cff09568b4dea3
maik10000 <91635577+maik10000@users.noreply.github.com>
2022-09-30 12:23:33 -0500

    listo

--- src/pagina-registro/pagina-registro.jsx
+++ src/pagina-registro/pagina-registro.jsx
@@ -1,17 +1,17 @@
 import './estiloRegistro.css'
-export function Pregistro(){
-    return(
+export function Pregistro() {
+    return (
         <div className='Registro'>
-                <form action="POST" className='form-registro'>
-                    <h2>Crea una cuenta</h2>
+            <form action="POST" className='form-registro'>
+                <h2>Crea una cuenta</h2>
                 <input type="text" placeholder='Nombre' />
                 <input type="text" placeholder='Apellido' />
                 <input type="text" placeholder='Correo electronico' />
                 <input type="text" placeholder='Numero Telefono' />
                 <input type="password" name='contraseña1' placeholder='Contraseña' />
                 <input type="password" name='contraseña2' placeholder='Confirmar contraseña' />
-                    <button>Registrarse</button>
-                </form>
+                <button>Registrarse</button>
+            </form>
         </div>
     )
 }
