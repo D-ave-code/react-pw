72cd8a95f84b99913860c30a1f5355efbcd00e5b
maik10000 <91635577+maik10000@users.noreply.github.com>
2022-10-02 17:01:08 -0500

    mmmm

--- src/pagina-sesion/iniciarSesion.jsx
+++ src/pagina-sesion/iniciarSesion.jsx
@@ -1,15 +1,45 @@
 import './estiloIniciarSesion.css'
-
+import { useState } from 'react'
 export function PSesion(){
-    
+    const [correo, setCorreo] = useState('')
+    const [pass, setPass] = useState('')
+    const iniciarSesion = (e) =>{
+        e.preventDefault()
+        const datos = {
+            email:correo,
+            password:pass
+        }
+        obtenerToken(datos)
+    }
+    const setMail= (e)=>{
+        setCorreo(e.target.value)
+    }
+    const setContra= (e)=>{
+        setPass(e.target.value)
+    }
     return(
         <div className='InicioSesion'>
-              <form action="POST" className='formulario-sesion'>
+              <form onSubmit={iniciarSesion} action="POST" className='formulario-sesion'>
               <h2>Iniciar Sesion</h2>
-                    <input type="text" value={mail} placeholder='Correo electronico' />
-                    <input type="password" placeholder='Contraseña' />
-                    <button>Iniciar</button>
+                    <input type="text" value={correo} onChange={setMail} placeholder='Correo electronico' />
+                    <input type="password" value={pass} onChange={setContra} placeholder='Contraseña' />
+                    <button type='submit'>Iniciar</button>
               </form>
         </div>
     )
+}
+const obtenerToken = async (datos)=>{
+  try{
+    const respuesta =  await fetch('https://unitbycode.com/lav-rea/public/api/auth/login',{
+        method:'POST',
+        body: JSON.stringify(datos),
+        headers:{
+            'Content-type':'application/json'
+        }
+})
+    const data =  await respuesta.json()
+    console.log(data)
+  }catch(e){
+    console.log(e)
+  }
 }
